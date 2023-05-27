005e59163a8b85949e4d27bca205dbf13ba9034c
PenguinsSiena <105580668+PenguinsSiena@users.noreply.github.com>
2023-05-27 10:53:51 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var mouseEvent = "Empty";
+var last_position_of_x, last_position_of_y;
+var last_position_of_touch_x, last_position_of_touch_y;
+var width = screen.width;
+
+new_screen_width = screen.width - 70;
+new_screen_height = screen.height - 300;
+
+if(width < 992){
+    document.getElementById("myCanvas").width = new_screen_width;
+    document.getElementById("myCanvas").height = new_screen_height;
+    document.body.style.overflow;
+}
+
 canvas =
 document.getElementById("myCanvas");
 ctx= canvas.getContext("2d");
@@ -60,8 +74,9 @@ function my_touchmove(e)
 }
 
 function my_touchstart(e){
-    last_position_of_touch_x = current_position_of_touch_x;
-    last_position_of_touch_y = current_position_of_touch_y;
+    
+    last_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
 }
 canvas.addEventListener("mousemove", my_mousemove);
 function my_mousemove(e);
